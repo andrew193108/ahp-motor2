26637a1d8472a32df5fda18acff1c999bc9523bf
andrew <andrewew1908@gmail.com>
2024-07-30 14:30:50 +0700

    tes berubah upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="jumbotron">
-        <h1 class="display-4">Selamat Datang di Sistem Pendukung Keputusan</h1>
+        <h1 class="display-4">Selamat Datang di Sistem Pendukung Keputusan x</h1>
         <p class="lead">Ini adalah sistem pendukung keputusan untuk pemilihan sepeda motor menggunakan metode AHP.</p>
         <hr class="my-4">
         <p>Anda dapat mengelola alternatif, kriteria, dan melihat hasil ranking dari menu di atas.</p>
